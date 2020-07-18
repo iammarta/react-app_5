a0a2590e606382fa1d134824f5382cb44f33ccfe
Marta Kozak <51427243+iammarta@users.noreply.github.com>
2020-07-18 16:51:22 +0300

    init: upload the project

---
+++ build/precache-manifest.b2b96387288f64dd4fd445ecc7dc4814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc5109f3ed69fda2e091",
+    "url": "/static/js/main.cc5109f3.chunk.js"
+  },
+  {
+    "revision": "7b5eb2e5e6d5471d2ffc",
+    "url": "/static/js/1.7b5eb2e5.chunk.js"
+  },
+  {
+    "revision": "cc5109f3ed69fda2e091",
+    "url": "/static/css/main.8f2cb0cc.chunk.css"
+  },
+  {
+    "revision": "7b5eb2e5e6d5471d2ffc",
+    "url": "/static/css/1.bd535568.chunk.css"
+  },
+  {
+    "revision": "e1d4a534811433c465b9ce50bca7faf2",
+    "url": "/index.html"
+  }
+];
